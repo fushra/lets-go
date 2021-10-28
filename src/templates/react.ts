173f3d63983e240e12bb3f67be289a0143334ee8
trickypr <trickypr@icloud.com>
2021-10-28 17:37:27 +1100

    ✨ Add react template

--- src/templates/react.ts
+++ src/templates/react.ts
@@ -1,6 +1,8 @@
-import { Category, TemplateBase } from './base'
+import { Category, Command, TemplateBase } from './base'
 
 export class ReactApp extends TemplateBase {
   name = 'React Webapp'
   protected category = [Category.WEB]
+
+  steps = [new Command('Create react app', 'npx', 'create-react-app', '.')]
 }
